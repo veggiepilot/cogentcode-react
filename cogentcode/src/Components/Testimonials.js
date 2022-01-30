c77f76ad6f2e4c60f643551be1607a329aec82b7
veggiepilot <johanneschitura@gmail.com>
2022-01-30 18:56:42 -0500

    Added header and testimonials background

--- cogentcode/src/Components/Testimonials.js
+++ cogentcode/src/Components/Testimonials.js
@@ -16,22 +16,22 @@ class Testimonials extends Component {
 
     return (
       <section id="testimonials">
-      <div className="text-container">
-         <div className="row">
+         <div className="text-container">
+            <div className="row">
 
-            <div className="two columns header-col">
-               <h1><span>Client Testimonials</span></h1>
-            </div>
+               <div className="two columns header-col">
+                  <h1><span>Client Testimonials</span></h1>
+               </div>
 
-            <div className="ten columns flex-container">
+               <div className="ten columns flex-container">
                   <ul className="slides">
-                      {testimonials}
+                     {testimonials}
                   </ul>
                </div>
             </div>
          </div>
-   </section>
-    );
+      </section>
+      );
   }
 }
 
