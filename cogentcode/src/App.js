9d8d0b9d0daf8e2bed352de9e358c4ea3cc3fca7
veggiepilot <johanneschitura@gmail.com>
2022-01-30 19:48:21 -0500

    Changed size of project grids

--- cogentcode/src/App.js
+++ cogentcode/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
-import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 
